205bf9d1583f867fe7c3c946e15d5a59cd86c353
linghuifang <834979382@qq.com>
2020-07-15 17:43:42 +0800

    my-blogs

---
+++ my-blog/src/components/ShowBlogs.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="show-blogs" v-theme:column="'narrow'" >
+    <h1>博客总览</h1>
+    <input type="text" v-model="search" placeholder="搜索">
+    <div v-for="blog in filteredBlogs" class="single-blog">
+      <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
+      <article>{{blog.body | snippet}}</article>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'show-blogs',
+  data () {
+    return {
+      blogs:[],
+      search:""
+    }
+  },
+  created(){
+    // this.$http.get("http://jsonplaceholder.typicode.com/posts")
+    // this.$http.get("../src/assets/posts.json")错误的
+    this.$http.get("./static/posts.json")
+    .then(function(data){
+      // console.log(data);
+      this.blogs = data.body.slice(0,10)
+      
+    })
+  },
+  computed:{
+    filteredBlogs(){
+     return this.blogs.filter((blog)=>{
+       return blog.title.match(this.search)
+     })
+    }
+  },
+  // 局部过滤
+  filters:{
+    // 'to-uppercase':function(value){
+    //   return value.toUpperCase()
+    // },
+    toUppercase(value){
+      return value.toUpperCase()
+    },
+
+  },
+  //自定义指令
+  directives:{
+    'rainbow':{
+      bind(el,binding,vnode){
+        el.style.color = '#'+Math.random().toString(16).slice(2,8)
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.show-blogs{
+  max-width:800px;
+  margin:0 auto;
+}
+.single-blog{
+  padding:20px;
+  margin:20px 0;
+  background:#eee;
+  box-sizing:border-box;
+}
+</style>
