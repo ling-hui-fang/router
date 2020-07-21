5b2da2de0cadb11a30f50fa33039834f42b68cfd
linghuifang <834979382@qq.com>
2020-07-21 16:20:11 +0800

    vcustomers

---
+++ vcustomers/src/components/Customers.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="customers container">
+    <Alert v-if="alert" v-bind:message="alert"></Alert> 
+    <h3 class="page-header">用户管理系统</h3>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>姓名</th>
+          <th>电话</th>
+          <th>邮箱</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="customer in customers">
+          <td>{{customer.name}}</td>
+          <td>{{customer.phone}}</td>
+          <td>{{customer.email}}</td>
+          <td>
+            <router-link v-bind:to="'/customer/'+customer.id" class="btn btn-default">详情</router-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import Alert from './Alert'
+export default {
+  name: 'customers',
+  components:{
+    Alert
+  },
+  data () {
+    return {
+     customers:[],
+     alert:""
+    }
+  },
+  methods:{
+    fetchCustomers(){
+      this.$http.get("http://localhost:3000/users")
+      .then(function(response){
+        this.customers = response.body;
+        // console.log(response)
+      })
+    }
+  },
+  created(){
+    if(this.$route.query.alert){
+      this.alert = this.$route.query.alert;
+    }
+    this.fetchCustomers()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ vcustomers/src/components/Edit.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="edit container">
+    <h1 class="page-header">编辑用户</h1>
+    <form v-on:submit="updateCustomer">
+      <div class="well">
+        <h4>用户信息</h4>
+        <div class="form-group">
+          <label>姓名</label>
+          <input type="text" class="form-control" v-model="customer.name" placeholder="name">
+        </div>
+        <div class="form-group">
+          <label>电话</label>
+          <input type="text" class="form-control" v-model="customer.phone" placeholder="phone">
+        </div>
+        <div class="form-group">
+          <label>邮箱</label>
+          <input type="text" class="form-control" v-model="customer.email" placeholder="email">
+        </div>
+        <div class="form-group">
+          <label>学历</label>
+          <input type="text" class="form-control" v-model="customer.education" placeholder="education">
+        </div>
+        <div class="form-group">
+          <label>毕业学校</label>
+          <input type="text" class="form-control" v-model="customer.graduationschool" placeholder="graduationschool">
+        </div>
+        <div class="form-group">
+          <label>职业</label>
+          <input type="text" class="form-control" v-model="customer.profession" placeholder="profession">
+        </div>
+        <div class="form-group">
+          <label>个人简介</label>
+          <textarea class="form-control" rows="10" v-model="customer.profile"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">添加</button>
+      </div>  
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'edit',
+  data () {
+    return {
+     customer:{}
+    }
+  },
+  methods:{
+    fetchCustomer(id){
+      this.$http.get("http://localhost:3000/users/"+id)
+      .then(function(response){
+        // console.log(response.body);
+        this.customer = response.body
+      })
+    },
+    updateCustomer(e){
+      if(!this.customer.name || !this.customer.phone || !this.customer.email){
+          console.log("请添加对应的信息");  
+      }else{
+        let updateCustomer = {
+          name:this.customer.name,
+          phone:this.customer.phone,
+          email:this.customer.email,
+          education:this.customer.education,
+          graduationschool:this.customer.graduationschool,
+          profession:this.customer.profession,
+          profile:this.customer.profile
+        }
+        this.$http.put("http://localhost:3000/users/"+this.$route.params.id,updateCustomer)
+        .then(function(response){
+          this.$router.push({path:"/",query:{alert:'用户信息跟新成功！'}})
+          console.log(response);
+          e.preventDefault()
+        })
+      }
+      e.preventDefault()
+    }
+    
+  },
+  created(){
+      this.fetchCustomer(this.$route.params.id)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
