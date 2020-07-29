0f34bea5b99db9d3ef6d09c75bd2cafe133586c7
linghuifang <834979382@qq.com>
2020-07-29 14:28:22 +0800

    主页菜单栏导航

--- vue-shop/src/components/Home.vue
+++ vue-shop/src/components/Home.vue
@@ -11,24 +11,30 @@
       <el-container>
         <!-- 导航菜单侧边栏 -->
         <el-aside width="200px">
-          <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航一</span>
-        </template>
-        <el-menu-item-group>
-          <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
-        </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
-        </el-menu-item-group>
-        <el-submenu index="1-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
-      </el-submenu>
+          <el-menu background-color="#333744" 
+                   text-color="#fff"
+                   active-text-color="#ffd04b"
+                  >
+            <!-- 一级菜单 -->
+            <el-submenu index="1">
+              <!-- 一级菜单的模板区域 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>导航一</span>
+              </template>
+
+                <!-- 二级菜单 -->
+                <el-menu-item index="1-4-1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                  </template>
+                </el-menu-item>
+                
+            </el-submenu>
+          </el-menu>
         </el-aside>
         <el-main>Main</el-main>
       </el-container>
