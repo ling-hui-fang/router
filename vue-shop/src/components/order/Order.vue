05f01cfa9d8ea05413955de449292df07d484917
linghuifang <834979382@qq.com>
2020-08-13 23:26:50 +0800

    新建order

---
+++ vue-shop/src/components/order/Order.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="order">
+         <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>订单管理</el-breadcrumb-item>
+            <el-breadcrumb-item>订单列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片视图区 -->
+        <el-card>
+            <el-row>
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+            </el-row>
+        </el-card>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'order',
+    data () {
+        return {
+        
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
+  
