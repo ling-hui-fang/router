444f4b075a9728242d907e758f2d0de98cb750d5
linghuifang <834979382@qq.com>
2020-08-14 15:17:57 +0800

    订单表格

--- vue-shop/src/components/order/Order.vue
+++ vue-shop/src/components/order/Order.vue
@@ -15,16 +15,121 @@
                     </el-input>
                 </el-col>
             </el-row>
+            <!-- 订单列表数据 -->
+            <el-table :data="orderlist" border stripe>
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="订单编号" prop="order_number"></el-table-column>
+                <el-table-column label="订单价格" prop="order_price"></el-table-column>
+                <el-table-column label="是否付款">
+                    <template slot-scope="scope">
+                        <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
+                        <el-tag type="danger" v-else>未付款</el-tag>
+                    </template>  
+                </el-table-column>
+                <el-table-column label="是否发货" prop="is_send"></el-table-column>
+                <el-table-column label="下单时间">
+                    <template slot-scope="scope">
+                        {{scope.row.create_time | dateFormat}}
+                    </template>   
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template slot-scope="scope">
+                        <el-button type="primary" icon="el-icon-edit" size="mini" @click="showBox"></el-button>
+                        <el-button type="success" icon="el-icon-location" size="mini"></el-button>
+                    </template>   
+                </el-table-column>
+            </el-table>
+            <!-- 分页 -->
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="queryInfo.pagesize"
+                :page-sizes="[5, 10, 15]"
+                :page-size="queryInfo.pagesize"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total">
+            </el-pagination>
         </el-card>
+        <!-- 修改文件的对话框 -->
+        <el-dialog title="提示" :visible.sync="addressVisible" width="50%" @close="addressDialogclosed">
+                <!-- 表单 -->
+                <el-form :model="addressForm" :rules="addressFormRules" ref="addressFormRef" label-width="100px">
+                    <el-form-item label="省市区/县" prop="address1">
+                        <el-cascader v-model="addressForm.address1" :options="cityData">
+
+                        </el-cascader>
+                    </el-form-item>
+                    <el-form-item label="详细地址" prop="address2">
+                        <el-input v-model="addressForm.address2"></el-input>
+                    </el-form-item>
+                </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="addressVisible = false">取 消</el-button>
+                <el-button type="primary" @click="addressVisible = false">确 定</el-button>
+            </span>
+        </el-dialog>
+
     </div>
 </template>
   
 <script>
-export default {
+import cityData from './cityData.js'
+export default {   
     name: 'order',
     data () {
         return {
-        
+            queryInfo:{
+                query:"",
+                pagenum:1,
+                pagesize:10
+            },
+            total:0,
+            orderlist:[],
+            addressVisible:false,
+            addressForm:{
+                address1:[],
+                address2:''
+            },
+            addressFormRules:{
+                address1:[
+                    { required: true, message: '请选择省市区/县', trigger: 'blur' },
+                ],
+                address2:[
+                    { required: true, message: '请填写详细地址', trigger: 'blur' },
+                ]
+            },
+            cityData
+        }
+    },
+    created(){
+        this.getOrderList()
+    },
+    methods:{
+       async getOrderList(){
+        const { data:res } = await this.$http.get('orders',{
+                params:this.queryInfo
+            })
+            if(res.meta.status !== 200){
+             this.$message.error('获取订单列表失败')
+            }
+            console.log(res);
+            this.orderlist = res.data.goods
+            this.total = res.data.total
+        },
+        handleSizeChange(newSize){
+            this.queryInfo.pagesize = newSize
+            this.getOrderList()
+        },
+        handleCurrentChange(newPage){
+            this.queryInfo.pagenum = newPage
+            this.getOrderList()
+        },
+        //展示修改框
+        showBox(){
+            this.addressVisible = true
+        },
+        addressDialogclosed(){
+            this.$refs.addressFormRef.resetFields()
         }
     }
 }
@@ -32,6 +137,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.el-cascader {
+    width:100%;
+}
 </style>
   
