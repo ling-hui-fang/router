7e07775f5d67adb452706c8fb813786292eacc84
linghuifang <834979382@qq.com>
2020-08-20 07:09:06 +0800

    ShowBlogs做了修改

--- my-blog/src/components/ShowBlogs.vue
+++ my-blog/src/components/ShowBlogs.vue
@@ -2,7 +2,7 @@
   <div class="show-blogs" v-theme:column="'narrow'" >
     <h1>博客总览</h1>
     <input type="text" v-model="search" placeholder="搜索">
-    <div v-for="blog in filteredBlogs" class="single-blog">
+    <div v-for="blog in filteredBlogs" class="single-blog" :key="blog.id">
       <router-link v-bind:to="'/blog/'+ blog.id">
         <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
       </router-link>
